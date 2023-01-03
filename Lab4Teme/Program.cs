214e5ce211df7f0ae3e181a5cdf80edbd0380618
Alexandru Tabara <alexandru.tab1@gmail.com>
2023-01-03 10:38:43 +0200

    Add project files.

---
+++ Lab4Teme/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace VectorInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Citeste marimea vectorului
+            Console.Write("Introdu marimea vectorului: ");
+            int n = int.Parse(Console.ReadLine());
+
+            //Creare de vector + adaugare de valori rand
+            int[] vector = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti valorea pentru elementul {i}: ");
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Apelare functie
+            InversareVector(vector);
+
+            //Afisare vector inversat
+            Console.WriteLine("Vector inversat:");
+            foreach(int value in vector)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+
+        static void InversareVector(int[] vector)
+        {
+            //Inverseaza elementele
+            int stanga = 0;
+            int dreapta = vector.Length - 1;
+            while (stanga < dreapta)
+            {
+                //Schimba valorile din stanga si dreapta
+                int temp = vector[stanga];
+                vector[stanga] = vector[dreapta];
+                vector[dreapta] = temp;
+
+                stanga++;
+                dreapta--;
+            }
+        }
+    }
+}
+
+ 
